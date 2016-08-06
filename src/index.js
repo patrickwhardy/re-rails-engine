51edbf5cbdc1ad25e3c9284fb9bf974b1988bf8d
patrickwhardy <patrickwhardy@gmail.com>
2016-08-06 10:29:23 -0600

    remove html from app.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Requests from './requests'
+
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+Requests.getMerchants();
